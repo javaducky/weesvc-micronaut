1e5d2a75ad9550e47893a539ae38e80e071fb60b
javaducky <javaducky@gmail.com>
2021-08-28 12:46:42 -0500

    Refresh project files based on latest Micronaut initializr

---
+++ src/test/java/io/weesvc/micronaut/weesvc/WeesvcTest.java
@@ -0,0 +1,21 @@
+package io.weesvc.micronaut.weesvc;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class WeesvcTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
